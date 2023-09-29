d4c275e11f394d0c040f0945dc1998af75508b47
Moyasi Ginko <mrsiyam61@gmail.com>
2023-09-30 03:47:12 +0600

    Populate db with trades

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+# Create a user
+user = User.create(
+  name: "John Doe",
+  role: "user"
+)
+
+# Create multiple trades associated with the user
+trades = [
+  {
+    name: "Trade 1",
+    description: "Description for Trade 1",
+    image: "trade1.jpg",
+    location: "Location 1",
+    price: 100.00,
+    duration: "1 hour",
+    trade_type: "Type 1",
+    user: user # Associate the trade with the user
+  },
+  {
+    name: "Trade 2",
+    description: "Description for Trade 2",
+    image: "trade2.jpg",
+    location: "Location 2",
+    price: 150.00,
+    duration: "2 hours",
+    trade_type: "Type 2",
+    user: user # Associate the trade with the user
+  }
+]
+
+Trade.create(trades)
