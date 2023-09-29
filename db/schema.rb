243228f6eef5ebec5c1a1fe9a45c3d9cfd132ac2
ibugithub <outsideworkibrahim@gmail.com>
2023-09-29 23:51:56 +0600

    remove some strange code

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_29_155755) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "messages", force: :cascade do |t|
-    t.string "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "reservations", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "trade_id", null: false
