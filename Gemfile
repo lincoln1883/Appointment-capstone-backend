13e266d73cb2dfd4ccd068e77d06e7476994fd89
ibugithub <outsideworkibrahim@gmail.com>
2023-09-29 22:13:42 +0600

    Add reservation migration file, initial model, controller, spec and factory skeletons

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ group :development, :test do
   gem 'factory_bot_rails'
   gem 'rails-controller-testing'
   gem 'rspec-rails', '~> 6.0.0'
+  gem 'dotenv-rails'
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_28_214341) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_29_155755) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "trade_id", null: false
+    t.date "date", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["trade_id"], name: "index_reservations_on_trade_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "trades", force: :cascade do |t|
     t.string "name"
@@ -35,5 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_28_214341) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reservations", "trades"
+  add_foreign_key "reservations", "users"
   add_foreign_key "trades", "users"
 end
