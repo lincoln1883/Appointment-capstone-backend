ab7e3b8f6ba6dc2aff9696c8ac29150f0b79e23b
Lincoln Gibson <lincolngibson7@gmail.com>
2023-10-07 16:15:07 -0300

    add validations and associations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :jwt_authenticatable, jwt_revocation_strategy: self
 
   validates :name, presence: true
+  has_many :reservations, dependent: :destroy
+  has_many :trades, through: :reservations
 
   before_create :set_default_role
   def set_default_role
