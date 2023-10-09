23b4ab5f0ee38d4f17b9653fcf229c2ea6148c8c
jic <ji_camargo@yahoo.com>
2023-10-09 18:35:24 -0500

    update file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
