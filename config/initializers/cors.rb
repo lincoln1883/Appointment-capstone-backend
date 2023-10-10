430b4f604fcbf4f4e9c5e02aad2e46bccd6ff004
Lincoln Gibson <lincolngibson7@gmail.com>
2023-10-09 21:34:18 -0300

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://handyhomehub.netlify.app/"
+    origins "https://handyhomehub.netlify.app"
 
     resource "*",
       headers: :any,
