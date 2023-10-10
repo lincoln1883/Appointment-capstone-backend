b8173a791f0acbfe8963c2402713a859398fd5c7
Lincoln Gibson <lincolngibson7@gmail.com>
2023-10-09 21:45:22 -0300

    Update fixes cors.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "https://handyhomehub.netlify.app"
-
     resource "*",
       headers: :any,
              expose: %w[access-token expiry token-type Authorization],
